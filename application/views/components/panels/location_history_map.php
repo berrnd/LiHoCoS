7d7fe941f961e9d4aadcc8713d56e6d2d21b47dd
Bernd Bestel <bernd@berrnd.de>
2014-06-22 19:04:08 +0200

    Added backward and forward buttons for daterange-picker in Location History

--- application/views/components/panels/location_history_map.php
+++ application/views/components/panels/location_history_map.php
@@ -2,12 +2,13 @@
     <div class="panel-heading">
         <span class="panel-title"><?php echo lang('Location History'); ?></span>
         <div class="pull-right">
+            <button style="margin-right: 5px;" type="button" class="btn btn-default btn-xs" onclick="window.lhmap.moveDateRange(true);"><i class="fa fa-backward fa-lg"></i></button>
             <span id="date-range-location-history-map">
                 <i class="fa fa-calendar fa-lg"></i>
                 <span></span> <i class="caret"></i>
             </span>
-            &nbsp;&nbsp;&nbsp;
-            <button type="button" class="btn btn-default btn-xs" onclick="window.lhmap.reload();"><i class="fa fa-refresh fa-lg"></i></button>
+            <button style="margin-left: 5px;" type="button" class="btn btn-default btn-xs" onclick="window.lhmap.moveDateRange(false);"><i class="fa fa-forward fa-lg"></i></button>
+            <button style="margin-left: 10px;" type="button" class="btn btn-default btn-xs" onclick="window.lhmap.reload();"><i class="fa fa-refresh fa-lg"></i></button>
         </div>
     </div>
     <div class="panel-body">
@@ -21,7 +22,7 @@
 
     window.lhmap = {};
     window.lhmap.daterange_start = moment().subtract('days', 1).startOf('day').format(MYSQL_DATETIME_FORMAT);
-    window.lhmap.daterange_end = moment().format(MYSQL_DATETIME_FORMAT);
+    window.lhmap.daterange_end = moment().subtract('days', 1).endOf('day').format(MYSQL_DATETIME_FORMAT);
 
     window.lhmap.reload = function() {
         window.lhmap.clear();
@@ -82,10 +83,10 @@
         for (i in window.lhmap.map._layers) {
             if (window.lhmap.map._layers[i]._path != undefined) {
                 try {
-                    m.removeLayer(m._layers[i]);
+                    window.lhmap.map.removeLayer(window.lhmap.map._layers[i]);
                 }
                 catch (e) {
-                    console.log("Error removing layer path " + e + m._layers[i]);
+                    console.log("Error removing layer path " + e + window.lhmap.map._layers[i]);
                 }
             }
         }
@@ -93,6 +94,26 @@
 
     window.lhmap.displayDateRange = function() {
         $('#date-range-location-history-map span').html(moment(window.lhmap.daterange_start).format('<?php echo lang('js_short_date_format'); ?>') + ' - ' + moment(window.lhmap.daterange_end).format('<?php echo lang('js_short_date_format'); ?>'));
+    }
+
+    window.lhmap.moveDateRange = function(backwards) {
+        var start = moment(window.lhmap.daterange_start);
+        var end = moment(window.lhmap.daterange_end);
+        var days = end.diff(start, 'days');
+
+        if (days == 0)
+            days = 1;
+
+        if (backwards)
+            days = days * -1;
+
+        start.add('days', days);
+        end.add('days', days);
+        window.lhmap.daterange_start = start.format(MYSQL_DATETIME_FORMAT);
+        window.lhmap.daterange_end = end.format(MYSQL_DATETIME_FORMAT);
+
+        window.lhmap.displayDateRange();
+        window.lhmap.reload();
     }
 
     $(document).ready(function() {
