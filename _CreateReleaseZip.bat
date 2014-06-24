a09014d61685772f17ce1f8380c9ec1011f1f08a
Bernd Bestel <bernd@berrnd.de>
2014-06-24 19:42:31 +0200

    Deployment enhancements

--- _CreateReleaseZip.bat
+++ _CreateReleaseZip.bat
@@ -1,14 +1,16 @@
 set projectPath=%~dp0
 set zip=A:\7-Zip\7za.exe
+set jq=A:\jq\jq.exe
 
 
 if %projectPath:~-1%==\ set projectPath=%projectPath:~0,-1%
 set myTemp=%temp%\lihocos_release%random%
-set /p version=<"%projectPath%\application\config\version.txt"
-
+for /F "delims=" %%i in ('type "%projectPath%\application\config\version.json" ^| "%jq%" .Build') do set version=%%i
 robocopy /MIR "%projectPath%" "%myTemp%" /XD .git /XD nbproject /XD demo.lihocos.de /XF .gitignore /XF README.md
 del "%myTemp%\_*.bat"
 
-"%zip%" a -tzip -r "%userprofile%\Desktop\LiHoCoS_%version%.zip" "%myTemp%\*.*"
+set releasePath=%projectPath%\_release
+mkdir "%releasePath%"
+"%zip%" a -tzip -r "%releasePath%\LiHoCoS_%version%.zip" "%myTemp%\*.*"
 
 rd /S /Q "%myTemp%"
